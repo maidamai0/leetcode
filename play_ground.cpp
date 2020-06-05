31ffea68cc9039b657ccbb6fbb79c3993302a34d
tonghao,yuan <yuantonghao@gmail.com>
2020-06-06 06:34:44 +0800

    fix: test ssh key

--- play_ground.cpp
+++ play_ground.cpp
@@ -3,40 +3,61 @@
 
 using namespace std;
 
-// Definition for singly-linked list.
 struct ListNode {
   int val;
   ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Definition for a Node.
+// Definition for a Node.
+class Node {
+public:
+  int val;
+  Node *next;
+  Node *random;
+
+  Node(int _val) {
+    val = _val;
+    next = NULL;
+    random = NULL;
+  }
 };
 
 class Solution {
 public:
-  ListNode *detectCycle(ListNode *head) {
-    auto *slow = head;
-    auto *fast = head;
-
-    bool has_cycle = false;
-    while (fast && fast->next) {
-      slow = slow->next;
-      fast = fast->next->next;
-
-      if (fast == slow) {
-        has_cycle = true;
-        break;
-      }
-    }
-
-    if (!has_cycle) {
+  Node *copyRandomList(Node *head) {
+    if (!head) {
       return nullptr;
     }
 
-    slow = head;
-    while (slow != fast) {
-      slow = slow->next;
-      fast = fast->next;
+    auto *cp = new Node(head->val);
+    cp->next = head->next;
+    cp->random = head->random;
+
+    auto tail = cp;
+
+    Node *tmp = nullptr;
+    while (head) {
+      tmp = new Node(head->val);
+      tmp->next = head->next;
+      tmp->random = head->random;
+      tail->next = tmp;
+
+      tail = tail->next;
+      head = head->next;
     }
 
-    return slow;
+    return cp;
   }
 };
+
+int main(int argc, char **argv) {
+  ListNode node(1);
+  node.next =
+      new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))));
+  Solution sln;
+  sln.oddEvenList(&node);
+}
